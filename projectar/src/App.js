7066682317321f9c0b59304939eb70e48f6bcf03
Prince-Jonathan <jonathannkansah13@gmail.com>
2020-06-17 09:49:58 +0100

    verified class component connection with flask

--- projectar/src/App.js
+++ projectar/src/App.js
@@ -1,13 +1,26 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
 import Menu from "./components/Menu";
 
-function App() {
-  const [name, setName] = useState({"name":"Jonathan"});
-  useEffect(() => {
-    fetch("/api").then((res) => res.json().then((data) => setName(data)));
-  });
-  return <Menu user={name} />;
-  // return <div>hello,there</div>;
-}
+// function App() {
+//   const [name, setName] = useState({"name":"Jonathan"});
+//   useEffect(() => {
+//     fetch("/api").then((res) => res.json().then((data) => setName(data)));
+//   });
+//   return <Menu user={name} />;
+//   // return <div>hello,there</div>;
+// }
 
+class App extends Component {
+  state = { name: "Jonathan" };
+  componentDidMount() {
+    this.fetch();
+  }
+  fetch=()=>{
+    return fetch("/api").then((res) => res.json().then((data) => this.setState(data)));
+  }
+  render() {
+    return <Menu user={this.state} />;
+  }
+}
 export default App;
