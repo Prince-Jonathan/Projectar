cd108d64a8eec05048c74834fd6457cf63cf979f
Prince-Jonathan <jonathannkansah13@gmail.com>
2020-06-20 18:48:31 +0100

    added styling alterations

--- projectar/src/App.js
+++ projectar/src/App.js
@@ -8,84 +8,10 @@ class App extends Component {
     return (
       <Row className="main">
         <Column flex="1" xs="1" className="aside">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa ratione
-          maxime recusandae? Voluptas perspiciatis laborum expedita hic
-          blanditiis eveniet veniam qui natus odit ratione itaque, adipisci
-          beatae dolorum dolores architecto. Lorem ipsum dolor sit amet
-          consectetur, adipisicing elit. Aperiam ducimus in porro autem, quo
-          cupiditate eum nostrum ex corrupti at eveniet atque saepe,
-          reprehenderit voluptate, cumque aut doloribus. Cum, velit!Lorem ipsum
-          dolor sit amet consectetur adipisicing elit. Ipsa ratione maxime
-          recusandae? Voluptas perspiciatis laborum expedita hic blanditiis
-          eveniet veniam qui natus odit ratione itaque, adipisci beatae dolorum
-          dolores architecto. Lorem ipsum dolor sit amet consectetur,
-          adipisicing elit. Aperiam ducimus in porro autem, quo cupiditate eum
-          nostrum ex corrupti at eveniet atque saepe, reprehenderit voluptate,
-          cumque aut doloribus. Cum, velit!Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Ipsa ratione maxime recusandae? Voluptas
-          perspiciatis laborum expedita hic blanditiis eveniet veniam qui natus
-          odit ratione itaque, adipisci beatae dolorum dolores architecto. Lorem
-          ipsum dolor sit amet consectetur, adipisicing elit. Aperiam ducimus in
-          porro autem, quo cupiditate eum nostrum ex corrupti at eveniet atque
-          saepe, reprehenderit voluptate, cumque aut doloribus. Cum, velit!Lorem
-          ipsum dolor sit amet consectetur adipisicing elit. Ipsa ratione maxime
-          recusandae? Voluptas perspiciatis laborum expedita hic blanditiis
-          eveniet veniam qui natus odit ratione itaque, adipisci beatae dolorum
-          dolores architecto. Lorem ipsum dolor sit amet consectetur,
-          adipisicing elit. Aperiam ducimus in porro autem, quo cupiditate eum
-          nostrum ex corrupti at eveniet atque saepe, reprehenderit voluptate,
-          cumque aut doloribus. Cum, velit!Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Ipsa ratione maxime recusandae? Voluptas
-          perspiciatis laborum expedita hic blanditiis eveniet veniam qui natus
-          odit ratione itaque, adipisci beatae dolorum dolores architecto. Lorem
-          ipsum dolor sit amet consectetur, adipisicing elit. Aperiam ducimus in
-          porro autem, quo cupiditate eum nostrum ex corrupti at eveniet atque
-          saepe, reprehenderit voluptate, cumque aut doloribus. Cum, velit!Lorem
-          ipsum dolor sit amet consectetur adipisicing elit. Ipsa ratione maxime
-          recusandae? Voluptas perspiciatis laborum expedita hic blanditiis
-          eveniet veniam qui natus odit ratione itaque, adipisci beatae dolorum
-          dolores architecto. Lorem ipsum dolor sit amet consectetur,
-          adipisicing elit. Aperiam ducimus in porro autem, quo cupiditate eum
-          nostrum ex corrupti at eveniet atque saepe, reprehenderit voluptate,
-          cumque aut doloribus. Cum, velit!Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Ipsa ratione maxime recusandae? Voluptas
-          perspiciatis laborum expedita hic blanditiis eveniet veniam qui natus
-          odit ratione itaque, adipisci beatae dolorum dolores architecto. Lorem
-          ipsum dolor sit amet consectetur, adipisicing elit. Aperiam ducimus in
-          porro autem, quo cupiditate eum nostrum ex corrupti at eveniet atque
-          saepe, reprehenderit voluptate, cumque aut doloribus. Cum, velit!Lorem
-          ipsum dolor sit amet consectetur adipisicing elit. Ipsa ratione maxime
-          recusandae? Voluptas perspiciatis laborum expedita hic blanditiis
-          eveniet veniam qui natus odit ratione itaque, adipisci beatae dolorum
-          dolores architecto. Lorem ipsum dolor sit amet consectetur,
-          adipisicing elit. Aperiam ducimus in porro autem, quo cupiditate eum
-          nostrum ex corrupti at eveniet atque saepe, reprehenderit voluptate,
-          cumque aut doloribus. Cum, velit!Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Ipsa ratione maxime recusandae? Voluptas
-          perspiciatis laborum expedita hic blanditiis eveniet veniam qui natus
-          odit ratione itaque, adipisci beatae dolorum dolores architecto. Lorem
-          ipsum dolor sit amet consectetur, adipisicing elit. Aperiam ducimus in
-          porro autem, quo cupiditate eum nostrum ex corrupti at eveniet atque
-          saepe, reprehenderit voluptate, cumque aut doloribus. Cum, velit!Lorem
-          ipsum dolor sit amet consectetur adipisicing elit. Ipsa ratione maxime
-          recusandae? Voluptas perspiciatis laborum expedita hic blanditiis
-          eveniet veniam qui natus odit ratione itaque, adipisci beatae dolorum
-          dolores architecto. Lorem ipsum dolor sit amet consectetur,
-          adipisicing elit. Aperiam ducimus in porro autem, quo cupiditate eum
-          nostrum ex corrupti at eveniet atque saepe, reprehenderit voluptate,
-          cumque aut doloribus. Cum, velit!Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Ipsa ratione maxime recusandae? Voluptas
-          perspiciatis laborum expedita hic blanditiis eveniet veniam qui natus
-          odit ratione itaque, adipisci beatae dolorum dolores architecto. Lorem
-          ipsum dolor sit amet consectetur, adipisicing elit. Aperiam ducimus in
-          porro autem, quo cupiditate eum nostrum ex corrupti at eveniet atque
-          saepe, reprehenderit voluptate, cumque aut doloribus. Cum, velit!
+         <Aside/>
         </Column>
         <Column flex="4" xs="4" className="section">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate
-          dolorum non similique impedit autem dolorem in quidem ex esse,
-          doloribus et veniam, sit aperiam adipisci distinctio voluptas soluta
-          quod quasi.
+          Dashboard
         </Column>
       </Row>
     );
