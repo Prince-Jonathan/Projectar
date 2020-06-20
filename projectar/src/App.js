d4cc584fe2d60c5fbd18b1a187d68d0c0849f400
Prince-Jonathan <jonathannkansah13@gmail.com>
2020-06-20 06:32:05 +0100

    initial commit

--- projectar/src/App.js
+++ projectar/src/App.js
@@ -1,15 +1,6 @@
-// import React, { useState, useEffect } from "react";
 import React, { Component } from "react";
 import Menu from "./components/Menu";
 
-// function App() {
-//   const [name, setName] = useState({"name":"Jonathan"});
-//   useEffect(() => {
-//     fetch("/api").then((res) => res.json().then((data) => setName(data)));
-//   });
-//   return <Menu user={name} />;
-//   // return <div>hello,there</div>;
-// }
 
 class App extends Component {
   state = { name: "Jonathan" };
