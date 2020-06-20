277c5d442400d42fb92e3c8417928d7e8692c682
Prince-Jonathan <jonathannkansah13@gmail.com>
2020-06-20 13:40:53 +0100

    initialize layouting

--- projectar/src/App.js
+++ projectar/src/App.js
@@ -1,10 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Aside from "./components/sidemenu/SideMenu";
+import "./App.css";
 
 class App extends Component {
-  render() { 
-    return ( <Aside/> );
+  render() {
+    return (
+      <div className="main">Main
+        <div className="aside">aside</div>
+        <div className="section">section</div>
+      </div>
+    );
   }
 }
- 
+
 export default App;
