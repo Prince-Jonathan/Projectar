bb32bccbb0e4b707365e8c4a29dc6fc2e8d209b4
Prince-Jonathan <jonathannkansah13@gmail.com>
2020-06-21 00:24:11 +0100

    composed a menulist of sidemenu items

--- projectar/src/components/sidemenu/Aside.jsx
+++ projectar/src/components/sidemenu/Aside.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import SideMenu, { Item } from "react-sidemenu";
-// import
+import SideMenu from "react-sidemenu";
+import items from "./menuList";
 import "./side-menu.css";
 
 class Aside extends Component {
@@ -13,44 +13,3 @@ class Aside extends Component {
   }
 }
 export default Aside;
-
-const items = [
-  { label: "Quick Add", value: "add", icon: "fa-plus-circle " },
-  { divider: true, label: "Main navigation", value: "main-nav" },
-  {
-    label: "Workspace",
-    value: "workspace",
-    icon: "fa-home",
-    children: [
-      {
-        label: "Add Role",
-        value: "addrole",
-        icon: "fa-snapchat",
-        children: [
-          { label: "item 1.1.1", value: "item1.1.1", icon: "fa-anchor" },
-          { label: "item 1.1.2", value: "item1.1.2", icon: "fa-bar-chart" },
-        ],
-      },
-      { label: "Add Person", value: "addperson" },
-    ],
-  },
-  {
-    label: "Projects",
-    value: "projects",
-    icon: "fa-suitcase",
-    children: [
-      {
-        label: "item 2.1",
-        value: "item2.1",
-        children: [
-          { label: "item 2.1.1", value: "item2.1.1" },
-          { label: "item 2.1.2", value: "item2.1.2" },
-        ],
-      },
-      { label: "item 2.2", value: "item2.2" },
-    ],
-  },
-  { label: "Reports", value: "reports", icon: "fa-book" },
-  { divider: true, label: "Others", value: "Others" },
-  { label: "Configuration", value: "configuration", icon: "fa-cogs" },
-];
