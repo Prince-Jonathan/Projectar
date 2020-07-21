925d6bed1e808f5f01886df8ab2c0b022bdf5dda
Prince-Jonathan <jonathannkansah13@gmail.com>
2020-07-21 14:56:19 +0000

    editted the wall

--- projectar/src/components/content/Wall.jsx
+++ projectar/src/components/content/Wall.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Wall = (props) => {
   const text = {
     color: "white",
-    fontFamily: "consolas, Open Sans, sans-serif",
+    fontFamily: "Open Sans, sans-serif",
     fontStyle: "italic",
   };
   let name = props.name ? `${props.name}'s ` : "";
