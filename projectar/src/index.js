950098fdd581c0e6a118790c54f30a316acd0ffe
Jonathan P. Nkansah <jnkansah@ppalgh.com>
2020-11-12 17:07:13 +0000

    miscellanous

--- projectar/src/index.js
+++ projectar/src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider as AlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
 import LoadingIndicator from "./components/loader/LoadingIndicator";
-// import ClearCache from "react-clear-cache";
 
 import App from "./App";
 
@@ -12,25 +11,6 @@ ReactDOM.render(
   // <React.StrictMode>
   <BrowserRouter>
     <AlertProvider template={AlertTemplate}>
-      {/* <ClearCache auto>
-        {({ isLatestVersion, emptyCacheStorage }) => (
-          <div>
-            {!isLatestVersion && (
-              <p>
-                <a
-                  href="#"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    emptyCacheStorage();
-                  }}
-                >
-                  Update version
-                </a>
-              </p>
-            )}
-          </div>
-        )}
-      </ClearCache> */}
       <App />
       <LoadingIndicator />
     </AlertProvider>
